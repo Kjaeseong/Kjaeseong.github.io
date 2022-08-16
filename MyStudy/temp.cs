a93da11d0d461dad5661f0b51ebbb226b172cd04
Kjaeseong <103081763+Kjaeseong@users.noreply.github.com>
2022-08-16 11:19:05 +0900

    .
    .

--- MyStudy/temp.cs
+++ MyStudy/temp.cs
@@ -73,3 +73,83 @@ class MainApp
 //(아이템 이름 : HP포션, 소지 개수 : 10개, 효과 : 체력을 채워준다.)
 //(아이템 이름 : MP포션, 소지 개수 : 8개, 효과 : 마나를 채워준다.)
 //(아이템 이름 : 분노 포션, 소지 개수 : 3개, 효과 : 일정 시간동안 공격력을 증가시킨다.)
+
+
+
+
+
+
+
+배열, 컬렉션, 인덱서
+
+배열 : 
+동시발사 탄환 수량제한(오브젝트풀 패턴) (확정)
+소환사 스킬 구현...? (동시에 소환수 4마리 넘으면 안된다던가..)
+
+컬렉션
+Stack - 유희왕 체인 개념
+Queue - 스타 건물에서 유닛 생산할때
+
+인덱서
+- 인벤토리 (확정)
+- 공격 쿨타임 제한
+
+
+
+
+
+
+
+class UnitA
+{
+    public float Hp;
+    public float MoveSpeed;
+    public float CreateTime = 20f;
+}
+
+class UnitB
+{
+    public float Hp;
+    public float MoveSpeed;
+    public float CreateTime = 30f;
+}
+
+class Factory
+{
+    Queue<GameObject> FactoryCreateUnit = new Queue<GameObject>();
+    public float GetTime = 0f;
+
+    private void Update() 
+    {
+        if(Input.GetKeyDown(KeyCode.A))
+        {
+            AddCreateUnit(UnitA);
+        }
+        if(Input.GetKeyDown(KeyCode.B))
+        {
+            AddCreateUnit(UnitB);
+        }
+
+        UnitProduct();
+    }
+
+    public void AddCreateUnit(GameObject unit)
+    {
+        FactoryCreateUnit.Enqueue(unit);
+    }
+
+    public void UnitProduct()
+    {
+        if(FactoryCreateUnit.Count != 0)
+        {
+            GetTime += Time.deltaTime;
+            if(FactoryCreateUnit.Peek().CreateTime <= GetTime)
+            {
+                FactoryCreateUnit.Dequeue;
+                GetTime = 0f;
+            }
+        }
+    }
+}
+
+
