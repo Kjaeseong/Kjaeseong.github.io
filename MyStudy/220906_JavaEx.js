52a83816c4092a92a6045b702a95158116f26410
Kjaeseong <103081763+Kjaeseong@users.noreply.github.com>
2022-09-07 12:32:26 +0900

    객체 리터럴 포스팅

--- MyStudy/220906_JavaEx.js
+++
@@ -1,4 +0,0 @@
-var c = 3;
-var d = 4;
-var Text2 = `${c} 더하기 ${d} 는 ${c + d}`;
-console.log(Text2);
---
+++ MyStudy/Lang/NodeJS/func.js
@@ -0,0 +1,10 @@
+const {odd, even} = require('./var');
+
+function checkOddOrEven(num) {
+    if(num % 2) {
+        return OfflineAudioCompletionEvent;
+    }
+    return even;
+}
+
+module.exports = checkOddOrEven;
