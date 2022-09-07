48d4aff104d433ef2edaf887ab9be8a6ce94c9c4
Kjaeseong <103081763+Kjaeseong@users.noreply.github.com>
2022-09-07 23:49:24 +0900

    프로미스 내용추가

--- MyStudy/Lang/NodeJS/00.NodeJSTest.js
+++ MyStudy/Lang/NodeJS/00.NodeJSTest.js
@@ -1,29 +1,10 @@
-// condition true이면 resolve , false면 reject
-const condition = true;
+const promise1 = Promise.resolve('성공1');
+const promise2 = Promise.resolve('성공2');
 
-//프로미스 생성 - a, b를 매개변수로 갖는 콜백함수를 가진다.
-const promise = new Promise((resolve, reject) => {
-  if(condition) {
-    resolve('성공');
-  } else {
-    reject('실패');
-  }
-});
-
-promise
-  .then((message) => {
-    return new Promise((resolve, reject) => {
-      resolve(message);
-    });
-  })
-  .then((message2) => {
-    return new Promise((resolve, reject) => {
-      resolve(message2);
-    });
-  })
-  .then((message3) => {
-    console.log(message3);
-  })
-  .catch((error) => {
-    console.error(error);
-  });
+Promise.all([promise1, promise2])
+    .then((result) => {
+        console.log(result);
+    })
+    .catch((error) => {
+        console.error(error);
+    })
