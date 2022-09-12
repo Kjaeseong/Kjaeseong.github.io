bb9d6c5ca0b13ff0da7424005963bd1f43468039
Kjaeseong <103081763+Kjaeseong@users.noreply.github.com>
2022-09-13 02:11:33 +0900

    백준 1110

--- CPP_test.cpp
+++ CPP_test.cpp
@@ -1,28 +1,20 @@
-bool BinarySearch(int* arr, int size, int value)
+#include <iostream>
+using namespace std;
+
+int main()
 {
-    //초기 범위
-    int start = 0;
-    int end = size;
+    int StartNum;
+    int x;
+    int Count = 0;
 
-    while(start < end)
+    cin >> StartNum;
+
+    x = StartNum;
+    do
     {
-        // 시작과 끝의 중간점 지정
-        int index = (start + end) / 2;
+        x = (x % 10 * 10) + ((x % 10 + x / 10) % 10);
+        Count++;
+    } while (x != StartNum);
 
-        if(arr[index] == value)
-        {
-            // 고른 곳이 원하는 데이터라면 true
-            return true;
-        }
-        else if(arr[index] < value)
-        {
-            // 고른 곳이 원하는 데이터보다 작다면 시작 위치 조정
-            start = index + 1;
-        }
-        else
-        {
-            // 고른 곳이 원하는 데이터보다 크다면 종료 위치 조정
-            end = index;
-        }
-    }
+    cout << Count;
 }
