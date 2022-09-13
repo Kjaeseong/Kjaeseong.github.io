c6173598113b911b369f87cea2da4dea009eb881
Kjaeseong <103081763+Kjaeseong@users.noreply.github.com>
2022-09-14 03:02:32 +0900

    힘들어......자고싶어...지금 새벽 3시...

--- JavaTest.js
+++ JavaTest.js
@@ -1,5 +1,21 @@
-const crypto = require('crypto');
+// server1-2.js
 
-console.log('base64: ', crypto.createHash('sha512').update('PW').digest('base64'));
-console.log('hex: ', crypto.createHash('sha512').update('PW').digest('hex'));
-console.log('base64: ', crypto.createHash('sha512').update('AnotherPW').digest('base64'));
+const http = require('http');
+
+http.createServer((req, res) => {
+    res.writeHead(200, { 'content-Type': 'text/html; charset=utf-8' });
+    res.write('<h1>hello Node1!</h1>');
+    res.end('<p>Hello Server1!</p>');
+})
+.listen(8080, () => {
+    console.log('8080포트 서버 대기 중');
+});
+
+http.createServer((req, res) => {
+    res.writeHead(200, { 'content-Type': 'text/html; charset=utf-8' });
+    res.write('<h1>hello Node2!</h1>');
+    res.end('<p>Hello Server2!</p>');
+})
+.listen(8081, () => {
+    console.log('8081포트 서버 대기 중');
+});
