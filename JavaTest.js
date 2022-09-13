30db6afe55bec71251961b120260c1bde30c1c78
Kjaeseong <103081763+Kjaeseong@users.noreply.github.com>
2022-09-14 02:06:36 +0900

    Update NodeJs QS

--- JavaTest.js
+++ JavaTest.js
@@ -1,21 +1,7 @@
-// server1-2.js
+const url = require('url');
+const querystring = require('querystring');
 
-const http = require('http');
-
-http.createServer((req, res) => {
-    res.writeHead(200, { 'content-Type': 'text/html; charset=utf-8' });
-    res.write('<h1>hello Node1!</h1>');
-    res.end('<p>Hello Server1!</p>');
-})
-.listen(8080, () => {
-    console.log('8080포트 서버 대기 중');
-});
-
-http.createServer((req, res) => {
-    res.writeHead(200, { 'content-Type': 'text/html; charset=utf-8' });
-    res.write('<h1>hello Node2!</h1>');
-    res.end('<p>Hello Server2!</p>');
-})
-.listen(8081, () => {
-    console.log('8081포트 서버 대기 중');
-});
+const parsedUrl = url.parse('http://www.hilbut.co.kr/?page=3&limit=10&category=nodejs&category=javascript');
+const query = querystring.parse(parsedUrl.query);
+console.log('querystring.parse():', query);
+console.log('querystring.stringify():', querystring.stringify(query));
